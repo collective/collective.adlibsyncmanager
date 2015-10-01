09cce56488d64414ff23071abd01a6df1ea4c76d
André Gonçalves <andreslb1@gmail.com>
2015-10-01 19:03:37 +0200

    change for persons total

--- collective/adlibsyncmanager/updater.py
+++ collective/adlibsyncmanager/updater.py
@@ -801,13 +801,13 @@ class Updater:
         self.warning_path_dev = "/Users/AG/Projects/collectie-zm/logs/warning_%s_%s.log" %(self.portal_type, str(timestamp))
         
         
-        collection_xml = person_single
+        collection_xml = persons_total
         if self.dev:
-            collection_xml = person_single
+            collection_xml = persons_total
             self.error_log_file = open(self.error_path_dev, "w+")
             self.warning_log_file = open(self.warning_path_dev, "w+")
         else:
-            collection_xml = person_single
+            collection_xml = persons_total
             self.error_log_file = open(self.error_path, "w+")
             self.warning_log_file = open(self.warning_path, "w+")
         
