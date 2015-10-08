437d303542c81ad71abdb4a8109bb0b2b22c1ca9
André Gonçalves <andreslb1@gmail.com>
2015-10-08 19:56:37 +0200

    Update entire collection

--- collective/adlibsyncmanager/updater.py
+++ collective/adlibsyncmanager/updater.py
@@ -55,7 +55,7 @@ from collective.object.utils.interfaces import INotes
 from z3c.relationfield import RelationValue
 from zope import component
 
-PORTAL_TYPE = "Resource"
+PORTAL_TYPE = "Object"
 
 if PORTAL_TYPE == "Object":
     from .core import CORE
@@ -977,7 +977,7 @@ class Updater:
         self.status_path_dev = "/Users/AG/Projects/collectie-zm/logs/status_%s_%s.csv" %(self.portal_type, str(timestamp))
         self.status_path = "/var/www/zm-collectie-v3/logs/status_%s_%s.csv" %(self.portal_type, str(timestamp))
         
-        collection_xml = resources_total
+        collection_xml = collection_total
         if self.dev:
             self.error_log_file = open(self.error_path_dev, "w+")
             self.warning_log_file = open(self.warning_path_dev, "w+")
@@ -999,7 +999,7 @@ class Updater:
         curr = 0
         limit = 0
 
-        for xml_record in list(self.collection)[-100:]:
+        for xml_record in list(self.collection):
             try:
                 curr += 1
                 transaction.begin()
