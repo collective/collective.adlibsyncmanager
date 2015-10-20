1e5e78e3be5274c0c1209d661ddceb1c193a07f6
André Gonçalves <andreslb1@gmail.com>
2015-10-20 17:49:36 +0200

    fix relations

--- collective/adlibsyncmanager/updater.py
+++ collective/adlibsyncmanager/updater.py
@@ -1007,8 +1007,8 @@ class Updater:
 
             if "object.object_number" in self.xml_path:
                 print "Number of values: "+str(len(value))
-                if len(value) > 170:
-                    value = value[:170]
+                if len(value) > 173:
+                    value = value[:173]
                 #print len(value)1
             setattr(plone_object, plone_fieldname, value)
         else:
