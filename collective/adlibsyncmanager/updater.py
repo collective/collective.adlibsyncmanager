abd102ee559aa886cba3317532b80f12fa231725
André Gonçalves <andreslb1@gmail.com>
2015-10-23 12:39:06 +0200

    Update taxonomies

--- collective/adlibsyncmanager/updater.py
+++ collective/adlibsyncmanager/updater.py
@@ -55,7 +55,7 @@ from collective.object.utils.interfaces import INotes
 from z3c.relationfield import RelationValue
 from zope import component
 
-PORTAL_TYPE = "PersonOrInstitution"
+PORTAL_TYPE = "Taxonomie"
 
 from .contenttypes_path import CONTENT_TYPES_PATH
 
@@ -1041,12 +1041,12 @@ class Updater:
             else:
                 if xml_path == "":
                     xml_path = xml_element.tag
-                    if (xml_path == "record") or ("parts_reference" in xml_path) or ("Child" in xml_path) or ("Synonym" in xml_path):
+                    if (xml_path == "record") or ("parts_reference" in xml_path):# or ("Child" in xml_path) or ("Synonym" in xml_path):
                         self.warning("%s__%s__Tag was ignored. %s" %(object_number, xml_path, xml_element.text))
                     else:
                         self.error("%s__%s__Tag not found in dictionary. %s" %(object_number, xml_path, xml_element.text))
                 else:
-                    if ("parts_reference" in xml_path) or ("Child" in xml_path) or ("Synonym" in xml_path):
+                    if ("parts_reference" in xml_path):# or ("Child" in xml_path) or ("Synonym" in xml_path):
                         self.warning("%s__%s__Tag was ignored. %s" %(object_number, xml_path, xml_element.text))
                     else:
                         self.error("%s__%s__Tag not found in dictionary. %s" %(object_number, xml_path, xml_element.text))
@@ -1312,11 +1312,9 @@ class Updater:
 
         self.init_log_files()
 
-        self.fix_persons_names()
-        return True
+        #self.fix_persons_names()
+        #return True
 
-       
-    
         #
         # Choose collection XML
         #
@@ -1332,7 +1330,7 @@ class Updater:
         curr, limit = 0, 0
         create_new = False
 
-        for xml_record in list(self.collection):
+        for xml_record in list(self.collection)[4100:4200]:
             try:
                 curr += 1
                 transaction.begin()
