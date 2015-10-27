ff957fb002fadf2ae82679f3e12f4c477083c10d
André Gonçalves <andreslb1@gmail.com>
2015-10-27 16:47:17 +0100

    Update all articles

--- collective/adlibsyncmanager/updater.py
+++ collective/adlibsyncmanager/updater.py
@@ -60,7 +60,7 @@ from collective.object.utils.interfaces import INotes
 from z3c.relationfield import RelationValue
 from zope import component
 
-PORTAL_TYPE = "Audiovisual"
+PORTAL_TYPE = "Article"
 
 from .contenttypes_path import CONTENT_TYPES_PATH
 
