dc1032945b39bd79c6b2fd338504fa941472af30
André Gonçalves <andreslb1@gmail.com>
2015-12-15 18:26:53 +0100

    Import all fossils

--- collective/adlibsyncmanager/TM/log_files_path.py
+++ collective/adlibsyncmanager/TM/log_files_path.py
@@ -4,23 +4,23 @@
 LOG_FILES_PATH = {
 	"import": {
 		"error": {
-			"dev":"/Users/AG/Projects/NewTeylersMuseum/logs/error/error_%s_%s.csv",
-			"prod":"/var/www/tm-logs/error/error_%s_%s.csv"
+			"dev":"/Users/AG/Projects/NewTeylersMuseum/logs/error/error_%s_%s_%s.csv",
+			"prod":"/var/www/tm-logs/error/error_%s_%s_%s.csv"
 		},
 		
 		"status": {
-			"dev":'/Users/AG/Projects/NewTeylersMuseum/logs/status/status_%s_%s.csv',
-			"prod":"/var/www/tm-logs/status/status_%s_%s.csv"
+			"dev":'/Users/AG/Projects/NewTeylersMuseum/logs/status/status_%s_%s_%s.csv',
+			"prod":"/var/www/tm-logs/status/status_%s_%s_%s.csv"
 		},
 
 		"warning": {
-			"dev":"/Users/AG/Projects/NewTeylersMuseum/logs/warning/warning_%s_%s.csv",
-			"prod":"/var/www/tm-logs/warning/warning_%s_%s.csv"
+			"dev":"/Users/AG/Projects/NewTeylersMuseum/logs/warning/warning_%s_%s_%s.csv",
+			"prod":"/var/www/tm-logs/warning/warning_%s_%s_%s.csv"
 		},
 
 		"images": {
-			"dev":"/Users/AG/Projects/NewTeylersMuseum/logs/images/images_%s_%s.csv",
-			"prod":"/var/www/tm-logs/images/images_%s_%s.csv"
+			"dev":"/Users/AG/Projects/NewTeylersMuseum/logs/images/images_%s_%s_%s.csv",
+			"prod":"/var/www/tm-logs/images/images_%s_%s_%s.csv"
 		}
 	},
 	"sync": {
