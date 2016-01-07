12605359037eff6b66856c51308e0fae80c3e593
André Gonçalves <andreslb1@gmail.com>
2016-01-07 15:42:37 +0100

    Translate 100 instrument

--- collective/adlibsyncmanager/TM/migrator.py
+++ collective/adlibsyncmanager/TM/migrator.py
@@ -914,12 +914,12 @@ class Migrator:
         curr, limit = 0, 0
         total = len(list(self.collection))
         
-        for xml_record in list(self.collection):
+        for xml_record in list(self.collection)[:100]:
             try:
                 transaction.begin()
                 curr += 1
                 priref = self.get_priref(xml_record)
-                if priref in TEST_EXAMPLES[self.object_type]:
+                if priref:
                     # Create translation
                     plone_object = self.find_object_by_priref(priref)
                     if plone_object:
